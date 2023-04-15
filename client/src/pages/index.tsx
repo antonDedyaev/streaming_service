f91fd91657525c60539ef4ef1cabf6024ca16990
eazaykova <lena.zaykova.95@gmail.com>
2023-04-15 13:57:56 +0700

    Added Actor

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,23 +1,33 @@
 import ButtonUI from "@/components/UI/Button/ButtonUI"
+
 import styles from '@/styles/pages/HomePage.module.scss'
 
+import ActorList from "@/components/actor/ActorList/ActorList";
+import { actors } from "@/components/actor/ActorList/Temp/Actors.data";
+
 function HomePage() {
-    return (
-        <>
-            <ButtonUI
-                className="button"
-                variant="large"
-                background="lightRed">
-                <div>Красная кнопка</div>
-            </ButtonUI>
-            <ButtonUI
-                className="button"
-                variant="medium"
-                background="gray">
-                <div>Серая кнопка</div>
-            </ButtonUI>
-        </>
-    )
+
+	return (
+		<>
+			<ButtonUI
+				className="button"
+				variant="large"
+				background="lightRed">
+				<div>Красная кнопка</div>
+			</ButtonUI>
+			<ButtonUI
+				className="button"
+				variant="medium"
+				background="gray">
+				<div>Серая кнопка</div>
+			</ButtonUI>
+
+
+			<ActorList actors={actors} effect={true} amt={true} size='Large' />
+			<ActorList actors={actors} amt={true} size='Medium' />
+			<ActorList actors={actors} role={true} size='Small' />
+		</>
+	)
 }
 
 export default HomePage
