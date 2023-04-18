5ccd6eda298cab1a6b36eea358b381268231857e
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-04-18 21:40:24 +0300

    rename components and change their logic

---
+++ client/src/components/UI/Spoiler/Spoiler.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SpoilerUI from './SpoilerUI';
+import { singleParagraph, moviePlot } from './storiesTemplates';
+import './SpoilerUI.module.scss';
+
+const meta: Meta<typeof SpoilerUI> = {
+    title: 'Spoiler/Spoiler',
+    component: SpoilerUI,
+    tags: ['autodocs'],
+    parameters: {
+        backgrounds: { default: 'dark' },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SpoilerUI>;
+
+export const EllipsedSpoiler: Story = {
+    args: {
+        children: singleParagraph,
+        toggleButtonTexts: ['Развернуть', 'Свернуть'],
+        buttonTextColor: 'bright',
+        shownLines: 2,
+        truncateFormat: 'vertical',
+    },
+};
+
+export const ClippedSpoiler: Story = {
+    args: {
+        children: moviePlot,
+        toggleButtonTexts: ['Детали о сериале', 'Свернуть детали'],
+        buttonTextColor: 'faded',
+        truncateFormat: 'horizontal',
+    },
+};
