38c8e717ef1aed2beaa96189dc4ed4af7bb134b3
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-05-01 23:52:11 +0300

    replace LinkUI with ShapedLinkUI in all components

--- client/src/components/movie/MovieAppeal/MovieAppeal.tsx
+++ client/src/components/movie/MovieAppeal/MovieAppeal.tsx
@@ -1,6 +1,6 @@
 import styles from './MovieAppeal.module.scss';
 import { IMovie } from '../movieMedallion/MovieMedallionsList/Temp/IMovie';
-import LinkUI from '@/components/UI/links/Link/LinkUI';
+import ShapedLinkUI from '@/components/UI/links/ShapedLink/ShapedLinkUI';
 
 interface MovieAppealProps {
     movie: IMovie;
@@ -11,9 +11,9 @@ const MovieAppeal = ({ movie }: MovieAppealProps) => {
         <div className={styles.container} data-testid="div-movieAppeal">
             <h2>Cмотреть «{movie.title}» на всех устройствах</h2>
             <p>Приложение доступно для скачивания на iOS, Android, SmartTV и приставках</p>
-            <LinkUI className={styles.container__link} href="https://www.ivi.ru/devices" shape="rectangular">
+            <ShapedLinkUI className={styles.container__link} href="https://www.ivi.ru/devices" shape="rectangular">
                 Подключить устройства
-            </LinkUI>
+            </ShapedLinkUI>
         </div>
     );
 };
