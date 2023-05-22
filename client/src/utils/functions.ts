5622ce6eb650e38ba148ddec9089ec79679aaf2d
eazaykova <lena.zaykova.95@gmail.com>
2023-05-22 15:48:16 +0700

    Added function minutesToHours and tests for it to files functions.ts and functions.test.ts

--- client/src/utils/functions.ts
+++ client/src/utils/functions.ts
@@ -1,9 +1,16 @@
 export const declineWord = (number: number, arrayOfOptions: string[]) => {
-  let dozens = number % 100;
-  let units = number % 10;
-  if (dozens > 10 && dozens < 20) return arrayOfOptions[2];
-  if (units > 1 && units < 5) return arrayOfOptions[1];
-  if (units == 1) return arrayOfOptions[0];
-  return arrayOfOptions[2];
-  /*тест*/
+    let dozens = number % 100;
+    let units = number % 10;
+    if (dozens > 10 && dozens < 20) return arrayOfOptions[2];
+    if (units > 1 && units < 5) return arrayOfOptions[1];
+    if (units == 1) return arrayOfOptions[0];
+    return arrayOfOptions[2];
+};
+
+export const minutesToHours = (minutes: number) => {
+    let m = minutes % 60;
+    let h = (minutes - m) / 60;
+    if (h <= 0) return `${(m < 10 ? '0' : '') + m.toString()} мин.`;
+    if (m <= 0) return `${h.toString()} ч.`;
+    return `${h.toString()} ч. ${(m < 10 ? '0' : '') + m.toString()} мин.`;
 };
