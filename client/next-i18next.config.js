6eb0381f1681d8c0dbc4ed81be9140a874030d51
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-07-05 12:15:29 +0300

    change i18next config and env variables

--- client/next-i18next.config.js
+++ client/next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
         locales: ['ru', 'en'],
         defaultLocale: 'ru',
+        localeDetection: false,
     },
+    localePath: path.resolve('./public/locales'),
 };
